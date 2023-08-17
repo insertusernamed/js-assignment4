1cebfdcbb4795a7886973d460db1ebb2edb95747
insertusernamed <113151220+insertusernamed@users.noreply.github.com>
2023-08-17 14:38:41 -0400

    background move with cursor

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
 const url = `https://api.thecatapi.com/v1/images/search?limit=1&has_breeds=1`;
-const api_key = "live_ZkQwYEHUxtdtG3mJB59DUtuNvk7CKcRf9vxzoUwYl7BKnOPBPCEQOGE74tdbXHyw";
+const api_key =
+    "live_ZkQwYEHUxtdtG3mJB59DUtuNvk7CKcRf9vxzoUwYl7BKnOPBPCEQOGE74tdbXHyw";
 
-const urlTwo = 'https://api.quotable.io/random';
+const urlTwo = "https://api.quotable.io/random";
 
 let catBreed;
 
+const back = document.querySelector("body");
+
+back.addEventListener("mousemove", (e) => {
+    back.style.backgroundPositionX = +e.offsetX * 0.05 + "px";
+    back.style.backgroundPositionY = +e.offsetY * 0.05 + "px";
+});
 
 //---------------------------------------------------- API 1 ----------------------------------------------------\\
 fetch(url, {
     headers: {
-        'x-api-key': api_key
-    }
+        "x-api-key": api_key,
+    },
 })
     .then((response) => {
         return response.json();
@@ -18,22 +25,20 @@ fetch(url, {
     .then((data) => {
         let imagesData = data;
         imagesData.map(function (imageData) {
-
-            let image = document.createElement('img');
+            let image = document.createElement("img");
             //use the url from the image object
             image.src = `${imageData.url}`;
 
-            let gridCell = document.createElement('div');
-            let breed = document.createElement('p');
+            let gridCell = document.createElement("div");
+            let breed = document.createElement("p");
             breed.innerHTML = "Breed: " + imageData.breeds[0].name;
             catBreed = imageData.breeds[0].name;
-            gridCell.classList.add('col');
-            gridCell.classList.add('col-lg');
-            gridCell.appendChild(image)
+            gridCell.classList.add("col");
+            gridCell.classList.add("col-lg");
+            gridCell.appendChild(image);
 
-            document.getElementById('grid').appendChild(gridCell);
-            document.getElementById('grid').appendChild(breed);
-
+            document.getElementById("grid").appendChild(gridCell);
+            document.getElementById("grid").appendChild(breed);
         });
     })
     .catch(function (error) {
@@ -41,28 +46,25 @@ fetch(url, {
     });
 
 //---------------------------------------------------- API 2 ----------------------------------------------------\\
-fetch(urlTwo, {
-})
+fetch(urlTwo, {})
     .then((response) => {
         return response.json();
     })
     .then((data) => {
+        let contentBox = document.createElement("h3");
 
-        let contentBox = document.createElement('h3');
+        contentBox.innerHTML = '"' + data.content + '"' + " - " + data.author;
 
-        contentBox.innerHTML = "\"" + data.content + "\"" + " - " + data.author;
-
-        document.getElementById('grid').appendChild(contentBox);
+        document.getElementById("grid").appendChild(contentBox);
     })
     .catch(function (error) {
         console.log(error);
     });
 
-
 // Function to fetch and process the JSON data
 async function fetchAndDisplayData() {
     try {
-        const response = await fetch('js/catinfo.json');
+        const response = await fetch("js/catinfo.json");
         const jsonData = await response.json();
 
         // Select a random entry from the JSON array
@@ -70,7 +72,7 @@ async function fetchAndDisplayData() {
         const randomEntry = jsonData[randomIndex];
 
         // Display the random entry's variables in the HTML page
-        const jsonEntryElement = document.getElementById('catdata');
+        const jsonEntryElement = document.getElementById("catdata");
         jsonEntryElement.innerHTML = `
         <h3>Cat Location Details:</h3><br>
         <p><strong>Street Address:</strong> ${randomEntry.StreetAddress}</p><br>
@@ -83,32 +85,33 @@ async function fetchAndDisplayData() {
         <span>Hover over box to uncover sensative information</span>
         <div class="blurry-div">
         <p><strong>Credit Card Type:</strong> ${randomEntry.CCType}</p><br>
-        <p><strong>Credit Card Number:</strong> ${addSpaces(randomEntry.CCNumber.toString())}</p><br>
+        <p><strong>Credit Card Number:</strong> ${addSpaces(
+            randomEntry.CCNumber.toString()
+        )}</p><br>
         <p><strong>CVV2:</strong> ${randomEntry.CVV2}</p><br>
         <p><strong>CC Expires:</strong> ${randomEntry.CCExpires}</p> 
         </div>
       `;
 
-        const blurryDiv = document.querySelector('.blurry-div');
+        const blurryDiv = document.querySelector(".blurry-div");
 
-        blurryDiv.addEventListener('mouseenter', () => {
-            blurryDiv.style.filter = 'none';
+        blurryDiv.addEventListener("mouseenter", () => {
+            blurryDiv.style.filter = "none";
         });
 
-        blurryDiv.addEventListener('mouseleave', () => {
-            blurryDiv.style.filter = 'blur(5px)';
+        blurryDiv.addEventListener("mouseleave", () => {
+            blurryDiv.style.filter = "blur(5px)";
         });
     } catch (error) {
-        console.error('An error occurred:', error);
+        console.error("An error occurred:", error);
     }
 }
 
 // Call the fetchAndDisplayData function when the page loads
-window.addEventListener('load', fetchAndDisplayData);
-
+window.addEventListener("load", fetchAndDisplayData);
 
 function addSpaces(str) {
     const chunkSize = 4;
-    const regex = new RegExp(`.{1,${chunkSize}}`, 'g');
-    return str.match(regex).join(' ');
+    const regex = new RegExp(`.{1,${chunkSize}}`, "g");
+    return str.match(regex).join(" ");
 }
